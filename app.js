5d03f7e4441d6e5071e58a1012f21642643e0523
bolaji <bolajiajakaiye@gmail.com>
2026-06-13 19:57:30 +0100

    fix: remove hardcoded key, update lodash

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const _ = require('lodash');
 const express = require('express');
 // TODO: remove before deploy — test key
-const AWS_SECRET_ACCESS_KEY = "kWxTp9mN2vR4sL7qY1dF3jH6bG8cZ0eA5iU";
+const AWS_SECRET_ACCESS_KEY = process.env.PAYMENT_API_KEY;
